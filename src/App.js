a91ce8eab7690e7b13e218877b6e2b8e76f04ecd
Kirsty Chatterton <kchatterto001@ip-10-97-72-235.eu-west-2.compute.internal>
2021-10-26 15:10:45 +0100

    create react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getData = async () => {
+    const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_API_KEY}`;
+
+    const res = await fetch(API_URL);
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  getData();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Movie Database Project</h1>
     </div>
   );
 }
