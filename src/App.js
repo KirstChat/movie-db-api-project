a35d97d878282028087ef46a048e94c74a836c36
Libby Henry <libbyh52@gmail.com>
2021-11-03 12:08:53 +0000

    Moved state for 'Search' component up to App level. Added in default render of 'original_name' if 'title' was missing from results

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 
 const App = () => {
 const [movies, setMovies] = useState([]);
+
 useEffect(() => {
   const getMovies = async() => {
     const res = await axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${process.env.REACT_APP_API_KEY}`)
@@ -14,12 +15,20 @@ useEffect(() => {
   }
   getMovies();
 }, [])
+
+const getContent = async searchValue => {
+  const res = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${searchValue}&language=en-US&page=1&include_adult=false`)
+  setMovies(res.data.results);
+}
+
 console.log(movies)
+
+
 
   return (
     <div>
-      <Header />
-      <Movies movies={movies} />
+      <Header getContent={getContent}/>
+       <Movies movies={movies} />
     </div>
   );
 }
--- src/Components/MovieItem.js
+++ src/Components/MovieItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './MovieItem.css';
 
 const MovieItem = props => {
-  const {title, poster_path, overview, vote_average} = props.movie;
+  const {title, original_name, poster_path, overview, vote_average} = props.movie;
   const prefix='https://image.tmdb.org/t/p/w500';
   return (
     <div className="movie-card">
@@ -15,9 +15,12 @@ const MovieItem = props => {
       <div className="rating">
           {`${vote_average*10}%`}
         </div> 
+        {title ?
         <h4 className="movie-title">
           {title}
-        </h4>
+        </h4> :
+        <h4>{original_name}</h4>
+        }
       </div>
       <div className="description">
         <p className="movie-overview">
