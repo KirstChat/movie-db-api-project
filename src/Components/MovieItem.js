0d154d9ff161e6df2cc5b415584994972518aa8b
Libby Henry <libbyh52@gmail.com>
2021-11-03 15:44:29 +0000

    Releveant class name added to second h4 in conditional. Ammended alt tag to use 'original_name' if 'title' missing. Additional styles added to component

--- src/Components/MovieItem.js
+++ src/Components/MovieItem.js
@@ -2,31 +2,30 @@ import React from 'react'
 import './MovieItem.css';
 
 const MovieItem = props => {
-  const {title, original_name, poster_path, overview, vote_average} = props.movie;
+  const {title, original_name, poster_path, vote_average} = props.movie;
   const prefix='https://image.tmdb.org/t/p/w500';
+
   return (
     <div className="movie-card">
+     <div className="img-container">
       <img 
-        className="movie-img"
-        src={`${prefix}${poster_path}`} 
-        alt={`poster for ${title}`}
-      />
+          className="movie-img"
+          src={`${prefix}${poster_path}`} 
+          alt={title ? `poster for ${title}`
+          : `poster for ${original_name}`}
+        />
+     </div>
       <div className="details">
-      <div className="rating">
-          {`${vote_average*10}%`}
-        </div> 
-        {title ?
-        <h4 className="movie-title">
-          {title}
-        </h4> :
-        <h4>{original_name}</h4>
-        }
-      </div>
-      <div className="description">
-        <p className="movie-overview">
-          {overview}
-        </p>
-      </div>    
+        <div className="rating">
+            {`${vote_average*10}%`}
+          </div> 
+          {title ?
+          <h4 className="movie-title">
+            {title}
+          </h4> :
+          <h4 className="movie-title">{original_name}</h4>
+          }
+        </div>   
     </div>
   )
 }
