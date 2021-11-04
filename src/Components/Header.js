4985bf45ae2a765c07f8b3a8f8f73c15099846f5
Libby Henry <libbyh52@gmail.com>
2021-11-04 09:34:20 +0000

    Basic responsiveness for movies container. Moved state from Header.js to App.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,12 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 import Form from './Form';
 
-const Header = () => {
-  const [content, setContent] = useState([])
-const getContent = async searchValue => {
-      const res = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${searchValue}&language=en-US&page=1&include_adult=false`)
-      setContent(res.data.results);
-    }
-    console.log(content);
+const Header = props => {
+
   return (
     <div>
       <h1>The Movie Database</h1>
-      <Form getContent={getContent}/>
+      <Form getContent={props.getContent}/>
     </div>
   )
 }
